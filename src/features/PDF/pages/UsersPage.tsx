394eadd6b9174aec42769bd83716b17c35c41f0a
Yonathan Gomez <jonathan.ursursoftware@gmail.com>
2025-09-09 16:22:20 -0500

    actualizacion de proyecto

--- src/features/PDF/pages/UsersPage.tsx
+++ src/features/PDF/pages/UsersPage.tsx
@@ -9,6 +9,7 @@ import { PdfCartaPorte } from '../components/PdfCartaPorte/PdfCartaPorte';
 import { AntiguedadSaldosPDF } from '../components/PdfAntiguedadSaldos/PdfAntiguedadSaldo';
 import {MiComponente} from "./../../typeScript/MyComponente";
 import {Dashboard}  from '../../EJERCICIOS_REACT/layout';
+import TodoApp from '../../EJERCICIOS_REACT/ToDoApp';
 // import Dashboard  from '../../EJERCICIOS_REACT/layout';
 
 
@@ -19,156 +20,18 @@ export const UsersPage = () => {
 //   console.log('Descargando PDF...');
 // };
 
-
-// const mockAntiguedadData: any[] = [
-//   {
-//     nombre_cliente: "ADN ENERGIA",
-//     documento: "E-15940",
-//     fecha: "22/05/2025",
-//     fecha_vencimiento: "22/05/2025",
-//     dias: 77,
-//     tipo: "Vencido",
-//     tipo_documento: "Directa",
-//     moneda: "MXN",
-//     tipo_cambio: 1.0,
-//     saldo: 73920.0,
-//     rango_1_7v: 0.0,
-//     rango_8_15v: 0.0,
-//     rango_16_30v: 0.0,
-//     mayor_31v: 73920.0,
-//     saldo_vencido: 73920.0,
-//     rango_1_7pv: 0.0,
-//     rango_8_15pv: 0.0,
-//     rango_16_30pv: 0.0,
-//     mayor_31pv: 0.0,
-//     saldo_por_vencer: 0.0,
-//   },
-//   {
-//     nombre_cliente: "ADN ENERGIA",
-//     documento: "E-15940",
-//     fecha: "22/05/2025",
-//     fecha_vencimiento: "22/05/2025",
-//     dias: 77,
-//     tipo: "Vencido",
-//     tipo_documento: "Directa",
-//     moneda: "MXN",
-//     tipo_cambio: 1.0,
-//     saldo: 73920.0,
-//     rango_1_7v: 0.0,
-//     rango_8_15v: 0.0,
-//     rango_16_30v: 0.0,
-//     mayor_31v: 73920.0,
-//     saldo_vencido: 73920.0,
-//     rango_1_7pv: 0.0,
-//     rango_8_15pv: 0.0,
-//     rango_16_30pv: 0.0,
-//     mayor_31pv: 0.0,
-//     saldo_por_vencer: 0.0,
-//   },
-//   {
-//     nombre_cliente: "ADN ENERGIA",
-//     documento: "E-15940",
-//     fecha: "22/05/2025",
-//     fecha_vencimiento: "22/05/2025",
-//     dias: 77,
-//     tipo: "Vencido",
-//     tipo_documento: "Directa",
-//     moneda: "MXN",
-//     tipo_cambio: 1.0,
-//     saldo: 73920.0,
-//     rango_1_7v: 0.0,
-//     rango_8_15v: 0.0,
-//     rango_16_30v: 0.0,
-//     mayor_31v: 73920.0,
-//     saldo_vencido: 73920.0,
-//     rango_1_7pv: 0.0,
-//     rango_8_15pv: 0.0,
-//     rango_16_30pv: 0.0,
-//     mayor_31pv: 0.0,
-//     saldo_por_vencer: 0.0,
-//   },
-//   {
-//     nombre_cliente: "ADN ENERGIA",
-//     documento: "E-15940",
-//     fecha: "22/05/2025",
-//     fecha_vencimiento: "22/05/2025",
-//     dias: 77,
-//     tipo: "Vencido",
-//     tipo_documento: "Directa",
-//     moneda: "MXN",
-//     tipo_cambio: 1.0,
-//     saldo: 73920.0,
-//     rango_1_7v: 0.0,
-//     rango_8_15v: 0.0,
-//     rango_16_30v: 0.0,
-//     mayor_31v: 73920.0,
-//     saldo_vencido: 73920.0,
-//     rango_1_7pv: 0.0,
-//     rango_8_15pv: 0.0,
-//     rango_16_30pv: 0.0,
-//     mayor_31pv: 0.0,
-//     saldo_por_vencer: 0.0,
-//   },
-//   {
-//     nombre_cliente: "ADN ENERGIA",
-//     documento: "E-15940",
-//     fecha: "22/05/2025",
-//     fecha_vencimiento: "22/05/2025",
-//     dias: 77,
-//     tipo: "Vencido",
-//     tipo_documento: "Directa",
-//     moneda: "MXN",
-//     tipo_cambio: 1.0,
-//     saldo: 73920.0,
-//     rango_1_7v: 0.0,
-//     rango_8_15v: 0.0,
-//     rango_16_30v: 0.0,
-//     mayor_31v: 73920.0,
-//     saldo_vencido: 73920.0,
-//     rango_1_7pv: 0.0,
-//     rango_8_15pv: 0.0,
-//     rango_16_30pv: 0.0,
-//     mayor_31pv: 0.0,
-//     saldo_por_vencer: 0.0,
-//   },
-//   {
-//     nombre_cliente: "ADN ENERGIA",
-//     documento: "E-15940",
-//     fecha: "22/05/2025",
-//     fecha_vencimiento: "22/05/2025",
-//     dias: 77,
-//     tipo: "Vencido",
-//     tipo_documento: "Directa",
-//     moneda: "MXN",
-//     tipo_cambio: 1.0,
-//     saldo: 73920.0,
-//     rango_1_7v: 0.0,
-//     rango_8_15v: 0.0,
-//     rango_16_30v: 0.0,
-//     mayor_31v: 73920.0,
-//     saldo_vencido: 73920.0,
-//     rango_1_7pv: 0.0,
-//     rango_8_15pv: 0.0,
-//     rango_16_30pv: 0.0,
-//     mayor_31pv: 0.0,
-//     saldo_por_vencer: 0.0,
-//   },
-// ];
-
 const frutas: string[] = ["banano", "manzana", "pera", "mora", "uva", "naranja"];
 
   return (
     <div className="user-card-grid">
       {/* <h1>Hola</h1> */}
 
-      <div>
-        <Dashboard />
-      </div>
+      <PDFViewer width= "150%" height="6000%"><CargaCombustiblePDF /></PDFViewer>
       {/* <div>
         <TodoApp frutas={frutas}/>
       </div> */}
       {/* <MiComponente/> */}
-      {/* <PDFViewer width= "300%" height="490%"><PdfPedidos /></PDFViewer> */}
+      {/* <PDFViewer width= "150%" height="490%"><PdfPedidos /></PDFViewer> */}
       {/* <PDFViewer width= "300%" height="490%"><PdfCartaPorte /></PDFViewer> */}
       {/* <PDFViewer width= "300%" height="490%"><AntiguedadSaldosPDF data={mockAntiguedadData} /></PDFViewer> */}
       {/* <PDFViewer width= "300%" height="490%"><MyDocument /></PDFViewer> */}
